afeaa22a1198ad8830bd036e85f3af1dfe58badd
Tomas Gandulfo <tomgandulfo@gmail.com>
2023-08-01 14:09:01 -0300

    Version 0.3
    Novedades:
    
    · Se agrego columna Tiempo a lista treeview y funcion a la misma.
    · Se agrego segundo separador debajo del switch de luz.
    · Se agrego boton y funcion para borrar entradas de la lista.
    · Se desactivo el strech de las columnas, causaba alargamiento del tab
    cuando se utilizaba el switch de luz.
    
    Errores:
    
    · Las columnas de la lista necesitan bugfixing, no se acoplan al final.
    
    Llamados de atencion:
    
    · Prestar atencion al entorno excell crea varios problemas siempre.
    · Atencion al boton de borrar seleccionados, no funciona como deberia.

--- main.py
+++ main.py
@@ -10,75 +10,6 @@ from openpyxl.utils import get_column_letter
 
 locale.setlocale(locale.LC_ALL, 'es_ES')
 
-def get_available_time_slots():
-    current_time = datetime.now()
-    end_of_day = current_time.replace(hour=23, minute=59)
-
-    time_slots = []
-    while current_time < end_of_day:
-        time_slots.append(current_time.strftime("%H:%M"))
-        current_time += timedelta(minutes=30)
-
-        # Saltar al próximo horario en punto si no está ya en punto
-        if current_time.minute != 0:
-            current_time += timedelta(minutes=(60 - current_time.minute))
-
-    return time_slots
-
-# Agregar datos al calendario
-def agregar_a_calendario():
-    # Obtener los datos ingresados en los widgets
-    banda = name_entry.get()
-    sala = sala_combobox.get()
-    abonado = varAux.get()
-    fecha = fechaReserva.get_date()
-    horario = hora_entry.get()  # Obtener el horario ingresado por el usuario
-
-    if varAux.get():
-        abonado = "Sí"
-    else:
-        abonado = "No"
-
-    # Validar que el nombre de la banda y la sala hayan sido ingresados
-    if not banda or banda == "Nombre de la banda":
-        tk.messagebox.showerror("Error", "Por favor, ingresa el nombre de la banda.")
-        return
-
-    if sala == "Seleccionar SALA":
-        tk.messagebox.showerror("Error", "Por favor, selecciona una sala.")
-        return
-    
-    if not horario or horario == "Horario":
-        tk.messagebox.showerror("Error", "Por favor, ingrese un horario.")
-        return
-
-    # Validar que todos los campos estén completos antes de agregar a la tabla
-    if banda and sala and fecha and horario:
-        lista.insert("", "end", values=(banda, sala, abonado, fecha, horario))
-
-        # Guardar los datos en la hoja de cálculo
-        guardar_en_excel(banda, sala, abonado, fecha, horario)
-
-        # Finalmente, limpiar los widgets para el siguiente ingreso
-        name_entry.delete(0, "end")
-        sala_combobox.set("Seleccionar SALA")
-        varAux.set(False)
-        fechaReserva.set_date(date.today())
-        hora_entry.delete(0, "end")  # Limpiar el Entry de horario
-
-    else:
-        # Mostrar un mensaje de error si falta algún campo (aunque las validaciones anteriores ya deberían cubrir esto)
-        tk.messagebox.showerror("Error", "Por favor, completa todos los campos.")
-
-# Modo oscuro toggle
-def toggle_mode():
-    if mode_switch.instate(["selected"]):
-        style.theme_use("forest-dark")
-    else:
-        style.theme_use("forest-light")
-
-    root.update()
-
 # Guardar archivo en Excel
 def guardar_en_excel(banda, sala, abonado, fecha, horario):
     archivo_excel = "calendario_bandas.xlsx"
@@ -100,8 +31,224 @@ def guardar_en_excel(banda, sala, abonado, fecha, horario):
     # Guardar los cambios en el archivo
     wb.save(archivo_excel)
 
+# Función para mostrar la pestaña "Otras Datos" cuando se hace clic en el botón
+def mostrar_otras_tab():
+    notebook.select(tab_datos)
+
+root = tk.Tk()
+root.title('CuzcoManager')
+
+style = ttk.Style(root)
+root.tk.call("source", "forest-light.tcl")
+root.tk.call("source", "forest-dark.tcl")
+style.theme_use("forest-light")
+
+listaSala_combo = ["Sala A", "Sala B", "Sala C", "Sala Z", "ESTUDIO"]
+
+class AgendaTab(ttk.Frame):
+    def __init__(self):
+        super().__init__()
+
+        # Crear un frame para contener los widgets de la columna 1
+        column1_frame = ttk.Frame(self)
+        column1_frame.grid(row=0, column=0, padx=5, pady=(0, 5), sticky="nsew")
+
+        #Nombre entry
+        self.name_entry = ttk.Entry(column1_frame)
+        self.name_entry.insert(0, "Nombre de la banda")
+        self.name_entry.bind("<FocusIn>", lambda e: self.name_entry.delete('0', 'end'))
+        self.name_entry.pack(fill="x", padx=5, pady=(10,5))
+
+        #Sala entry
+        self.sala_combobox = ttk.Combobox(column1_frame, values=listaSala_combo)
+        self.sala_combobox.insert(0, "Seleccionar SALA")
+        self.sala_combobox.pack(fill="x", padx=5, pady=(0,5))
+
+        #EstaPago? Entry
+        self.varAux = tk.BooleanVar()
+        checkbutton = ttk.Checkbutton(column1_frame, text="Abonado", variable=self.varAux)
+        checkbutton.pack(fill="x", padx=5, pady=(0,5))
+
+        #Dia entry
+        self.fechaReserva = tkcalendar.DateEntry(column1_frame, locale='es_ES', date_pattern='dd/MM/yyyy')
+        self.fechaReserva.pack(fill="x", padx=5, pady=(0,5))
+
+        #Widget Horario
+        self.hora_entry = ttk.Entry(column1_frame)
+        self.hora_entry.insert(0, "Horario")  # Texto de ejemplo en el Entry
+        self.hora_entry.bind("<FocusIn>", lambda e: self.hora_entry.delete('0', 'end'))
+        self.hora_entry.pack(fill="x", padx=5, pady=(0,5))
+
+        # Crear el combobox para la columna "Tiempo"
+        self.tiempo_combobox = ttk.Combobox(column1_frame, values=["1 hora", "2 horas", "3 horas", "4 horas"])
+        self.tiempo_combobox.insert(0, "Seleccionar tiempo")
+        self.tiempo_combobox.pack(fill="x", padx=5, pady=(0, 5))
+
+        #Boton para insertar a calendario
+        botonInsACalendario = ttk.Button(column1_frame, text="Agregar a calendario", command=self.agregar_a_calendario)
+        botonInsACalendario.pack(fill="x", padx=5, pady=(0,5))
+
+        #Separador1
+        separator1 = ttk.Separator(column1_frame)
+        separator1.pack(fill="x", padx=(10, 10), pady=10)
+
+        #ModoGui
+        self.mode_switch = ttk.Checkbutton(
+            column1_frame, text="Dia/Noche", style="Switch", command=self.toggle_mode)
+        self.mode_switch.pack(fill="x", padx=5, pady=10)
+
+        #Separador2
+        separator2 = ttk.Separator(column1_frame)
+        separator2.pack(fill="x", padx=(10, 10), pady=10)
+
+        # Botón para borrar entradas seleccionadas
+        botonBorrar = ttk.Button(column1_frame, text="Borrar seleccionados", command=self.borrar_seleccionados)
+        botonBorrar.pack(fill="x", padx=5, pady=(0,5))
+
+        # Treeview para mostrar los datos
+        treeFrame = ttk.Frame(self)
+        treeFrame.grid(row=0, column=2, padx=10, pady=10, columnspan=1, sticky="nsew")
+        treeScroll = ttk.Scrollbar(treeFrame)
+        treeScroll.pack(side="right", fill="y")
+        treeFrame.columnconfigure(0, weight=1)
+
+        cols = ("Banda", "Sala", "Abonado", "Fecha", "Horario", "Tiempo")
+        self.lista = ttk.Treeview(treeFrame, show="headings", yscrollcommand=treeScroll.set, columns=cols, height=13)
+
+        # Configurar los encabezados de las columnas
+        self.lista.heading("Banda", text="Banda")
+        self.lista.heading("Sala", text="Sala")
+        self.lista.heading("Abonado", text="Abonado")
+        self.lista.heading("Fecha", text="Fecha")
+        self.lista.heading("Horario", text="Horario")
+        self.lista.heading("Tiempo", text="Tiempo")
+
+        self.lista.column("Banda", width=120, anchor = "center", stretch=False)
+        self.lista.column("Sala", width=100, anchor = "center", stretch=False)
+        self.lista.column("Abonado", width=55, anchor = "center", stretch=False)
+        self.lista.column("Fecha", width=75, anchor = "center", stretch=False)
+        self.lista.column("Horario", width=50, anchor = "center", stretch=False)
+        self.lista.column("Tiempo", width=50, anchor = "center", stretch=False)
+
+        treeScroll.config(command=self.lista.yview)
+        self.lista.pack(expand=True, fill="both")
+
+        self.style = ttk.Style(self)
+
+    def borrar_seleccionados(self):
+        selected_items = self.lista.selection()
+        if not selected_items:
+            tk.messagebox.showinfo("Advertencia", "Por favor, selecciona al menos una entrada para borrar.")
+            return
+
+        # Preguntar al usuario si realmente quiere borrar las entradas seleccionadas
+        confirmacion = tk.messagebox.askyesno("Confirmar", "¿Estás seguro de que quieres borrar las entradas seleccionadas?")
+        if not confirmacion:
+            return
+
+        # Eliminar las entradas seleccionadas del widget de lista
+        for item in selected_items:
+            self.lista.delete(item)
+
+        # Guardar los cambios en el archivo Excel
+        archivo_excel = "calendario_bandas.xlsx"
+        if os.path.exists(archivo_excel):
+            wb = openpyxl.load_workbook(archivo_excel)
+            ws = wb.active
+
+            # Obtener los datos actuales del archivo Excel
+            data = []
+            for row in ws.iter_rows(values_only=True):
+                data.append(row)
+
+            # Eliminar las entradas seleccionadas de los datos
+            new_data = [row for row in data if tuple(row) not in selected_items]
+
+            # Borrar el contenido existente en el archivo Excel
+            ws.delete_rows(2, ws.max_row)
+
+            # Agregar los datos actualizados al archivo Excel
+            for row in new_data:
+                ws.append(row)
+
+            # Guardar los cambios en el archivo
+            wb.save(archivo_excel)
+
+    # Agregar datos al calendario
+    def agregar_a_calendario(self):
+        # Obtener los datos ingresados en los widgets
+        banda = self.name_entry.get()
+        sala = self.sala_combobox.get()
+        abonado = self.varAux.get()
+        fecha = self.fechaReserva.get_date()
+        horario = self.hora_entry.get()
+        tiempo = self.tiempo_combobox.get()
+
+        if self.varAux.get():
+            abonado = "Sí"
+        else:
+            abonado = "No"
+
+        # Validar que el nombre de la banda y la sala hayan sido ingresados
+        if not banda or banda == "Nombre de la banda":
+            tk.messagebox.showerror("Error", "Por favor, ingresa el nombre de la banda.")
+            return
+
+        if sala == "Seleccionar SALA":
+            tk.messagebox.showerror("Error", "Por favor, selecciona una sala.")
+            return
+        
+        if not horario or horario == "Horario":
+            tk.messagebox.showerror("Error", "Por favor, ingrese un horario.")
+            return
+
+        if tiempo == "Seleccionar tiempo":
+            tk.messagebox.showerror("Error", "Por favor, selecciona un tiempo.")
+            return
+
+        # Validar que todos los campos estén completos antes de agregar a la tabla
+        if banda and sala and fecha and horario and tiempo:
+            tab_agenda.lista.insert("", "end", values=(banda, sala, abonado, fecha, horario, tiempo))
+
+            # Guardar los datos en el hoja de cálculo
+            guardar_en_excel(banda, sala, abonado, fecha, horario, tiempo)
+
+            # Finalmente, limpiar los widgets para el siguiente ingreso
+            tab_agenda.name_entry.delete(0, "end")
+            tab_agenda.sala_combobox.set("Seleccionar SALA")
+            tab_agenda.varAux.set(False)
+            tab_agenda.fechaReserva.set_date(date.today())
+            tab_agenda.hora_entry.delete(0, "end")
+            tab_agenda.tiempo_combobox.set("Seleccionar tiempo")  # Limpiar el combobox de tiempo
+        else:
+            # Mostrar un mensaje de error si falta algún campo
+            tk.messagebox.showerror("Error", "Por favor, completa todos los campos.")
+
+    # Modo oscuro toggle
+    def toggle_mode(self):
+        if self.mode_switch.instate(["selected"]):
+            self.style.theme_use("forest-dark")
+        else:
+            self.style.theme_use("forest-light")
+
+        self.root.update()
+
+# Crear el Notebook para las pestañas
+frame = ttk.Frame(root)
+frame.pack()
+notebook = ttk.Notebook(frame)
+notebook.grid(row=0, column=0, padx=20, pady=10, columnspan=2, sticky="nsew")
+
+# Pestaña Actual
+tab_agenda = AgendaTab()
+notebook.add(tab_agenda, text="Agenda de bandas")
+
+# Pestaña Otras Datos
+tab_datos = ttk.Frame(notebook)
+notebook.add(tab_datos, text="Otras Datos")
+
 # Función para cargar datos desde el archivo Excel y mostrarlos en el widget de listado
-def cargar_datos_en_listado():
+def cargar_datos_en_listado(tab):
     archivo_excel = "calendario_bandas.xlsx"
 
     if not os.path.exists(archivo_excel):
@@ -115,101 +262,26 @@ def cargar_datos_en_listado():
     # Leer los datos de cada fila y almacenarlos en una lista
     data = []
     for row in ws.iter_rows(values_only=True):
-        banda, sala, abonado, fecha, horario = row
-        data.append((banda, sala, abonado, fecha, horario))
+        # Verificar que la fila tenga al menos cinco elementos (banda, sala, abonado, fecha y horario)
+        if len(row) >= 5:
+            banda, sala, abonado, fecha, horario = row
+            # Verificar si todas las celdas necesarias contienen información
+            if banda and sala and abonado and fecha and horario:
+                # Verificar si la fecha es válida antes de formatearla
+                if fecha:
+                    fecha_formateada = fecha.strftime('%d/%m/%Y')
+                else:
+                    fecha_formateada = ""
+                data.append((banda, sala, abonado, fecha_formateada, horario))
 
-    # Limpiar el contenido actual del widget de listado
-    lista.delete(*lista.get_children())
+    # Limpiar el contenido actual del widget de listado en la pestaña específica
+    tab.lista.delete(*tab.lista.get_children())
 
-    # Insertar los datos en el widget de listado
+    # Insertar los datos en el widget de listado en la pestaña específica
     for row in data:
-        lista.insert("", "end", values=row)
-
-root = tk.Tk()
-root.title('CuzcoManager')
-
-
-style = ttk.Style(root)
-root.tk.call("source", "forest-light.tcl")
-root.tk.call("source", "forest-dark.tcl")
-style.theme_use("forest-light")
-
-
-listaSala_combo = ["Sala A", "Sala B", "Sala C", "Sala Z", "ESTUDIO"]
-
-frame = ttk.Frame(root)
-frame.pack()
-
-widgets_frame = ttk.LabelFrame(frame, text="Agendar Banda")
-widgets_frame.grid(row=0, column =0, padx=20, pady=10)
-
-#Nombre entry
-name_entry = ttk.Entry(widgets_frame)
-name_entry.insert(0, "Nombre de la banda")
-name_entry.bind("<FocusIn>", lambda e: name_entry.delete('0', 'end'))
-name_entry.grid(row=0, column=0, padx=5, pady=(0, 5), sticky="ew")
-
-#Sala entry
-sala_combobox = ttk.Combobox(widgets_frame, values=listaSala_combo)
-sala_combobox.insert(0, "Seleccionar SALA")
-#sala_combobox.bind("<FocusIn>", lambda e: sala_combobox.delete('0', 'end'))
-sala_combobox.grid(row=1, column=0, padx=5, pady=(0, 5), sticky="ew")
-
-#EstaPago? Entry
-varAux = tk.BooleanVar()
-checkbutton = ttk.Checkbutton(widgets_frame, text="Abonado", variable=varAux)
-checkbutton.grid(row=2, column=0, padx=5, pady=(0, 5), sticky="nsew")
-
-#Dia entry
-fechaReserva = tkcalendar.DateEntry(widgets_frame, locale='es_ES', date_pattern='dd/MM/yyyy')
-fechaReserva.grid(row=3, column=0, padx=5, pady=(0, 5), sticky="nsew")
-
-#Widget Horario
-hora_entry = ttk.Entry(widgets_frame)
-hora_entry.insert(0, "Horario")  # Texto de ejemplo en el Entry
-hora_entry.bind("<FocusIn>", lambda e: hora_entry.delete('0', 'end'))
-hora_entry.grid(row=4, column=0, padx=5, pady=(0, 5), sticky="ew")
-
-#Boton para insertar a calendario
-botonInsACalendario = ttk.Button(widgets_frame, text="Agregar a calendario", command=agregar_a_calendario)
-botonInsACalendario.grid(row=5, column=0, padx=5, pady=(0, 5))
-
-#Calendario
-
-treeFrame = ttk.Frame(frame)
-treeFrame.grid(row=0, column=1, pady=10)
-treeScroll = ttk.Scrollbar(treeFrame)
-treeScroll.pack(side="right", fill="y")
-
-cols = ("Banda", "Sala", "Abonado", "Fecha", "Horario")
-lista = ttk.Treeview(treeFrame, show="headings",
-                        yscrollcommand=treeScroll.set, columns=cols, height=13)
-
-# Configurar los encabezados de las columnas
-lista.heading("Banda", text="Banda")
-lista.heading("Sala", text="Sala")
-lista.heading("Abonado", text="Abonado")
-lista.heading("Fecha", text="Fecha")
-lista.heading("Horario", text="Horario")
-
-lista.column("Banda", width=100)
-lista.column("Sala", width=100)
-lista.column("Abonado", width=50)
-lista.column("Fecha", width=100)
-lista.column("Horario", width=50)
-lista.pack()
-treeScroll.config(command=lista.yview)
-
-#Separador
-separator = ttk.Separator(widgets_frame)
-separator.grid(row=6, column=0, padx=(20, 10), pady=10, sticky="ew")
-
-#ModoGui
-mode_switch = ttk.Checkbutton(
-    widgets_frame, text="Dia/Noche", style="Switch", command=toggle_mode)
-mode_switch.grid(row=7, column=0, padx=5, pady=10, sticky="nsew")
+        tab.lista.insert("", "end", values=row)
 
 #CargadodeDatos
-cargar_datos_en_listado()
+cargar_datos_en_listado(tab_agenda)
 
 root.mainloop()
